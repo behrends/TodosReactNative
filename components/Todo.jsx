d2432ef6bf6f2e3bc43a666281b961e8ce91d2d4
Erik Behrends <behrends@users.noreply.github.com>
2024-10-14 17:17:58 +0200

    Todo und Checkbox mit state

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'; // <-- import aus react!
 import { StyleSheet, Text, View } from 'react-native';
 import Checkbox from 'expo-checkbox';
 
 export default function Todo({ children }) {
+  const [done, setDone] = useState(false); // <-- state mit false initialisieren
+
+  // Der Rest bleibt gleich
   return (
     <View style={styles.container}>
-      <Checkbox style={styles.checkbox} />
+      <Checkbox
+        style={styles.checkbox}
+        value={done} // <-- Verwendung des state
+        onValueChange={setDone} // <-- Verwendung der update function
+      />
       <Text style={styles.todoText}>{children}</Text>
     </View>
   );
