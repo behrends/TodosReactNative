1c39667b86b5bbc10da6f10df4cdac7d205b0a4f
Erik Behrends <behrends@users.noreply.github.com>
2024-10-14 17:24:01 +0200

    Todo-Text antippbar und durchgestrichen falls erledigt

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'; // <-- import aus react!
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Checkbox from 'expo-checkbox';
 
 export default function Todo({ children }) {
   const [done, setDone] = useState(false); // <-- state mit false initialisieren
 
-  // Der Rest bleibt gleich
+  const doneStyle = done
+    ? { textDecorationLine: 'line-through' }
+    : {}; // <-- Styling abhängig vom state
   return (
-    <View style={styles.container}>
-      <Checkbox
-        style={styles.checkbox}
-        value={done} // <-- Verwendung des state
-        onValueChange={setDone} // <-- Verwendung der update function
-      />
-      <Text style={styles.todoText}>{children}</Text>
-    </View>
+    <Pressable onPress={() => setDone(!done)}>
+      <View style={styles.container}>
+        <Checkbox
+          style={styles.checkbox}
+          value={done} // <-- Verwendung des state
+          onValueChange={setDone} // <-- Verwendung der update function
+        />
+        <Text style={[styles.todoText, doneStyle]}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
 
